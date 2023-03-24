49b3bea23c750d3d01feb14614a63c6897b24127
ManuelEscritorio <menuel.latorre@est.fi.uncoma.edu.ar>
2023-03-24 13:21:17 -0300

    a

---
+++ LedPrendeApaga/main.c
@@ -0,0 +1,14 @@
+int main(void)
+{
+  volatile unsigned char * DDR_B = (unsigned char *) 0x24;
+  volatile unsigned char * PUERTO_B = (unsigned char *) 0X25;
+  *(DDR_B)=32;
+  volatile long i=0;
+  while(1){
+    *(PUERTO_B) = 32;
+    for(i=0; i<100000; i++){} //delay prendido
+    *(PUERTO_B) = 0;
+    
+    for(i=0; i<100000; i++){} //delay apagado
+  }
+}
