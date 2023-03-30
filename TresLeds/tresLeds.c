4dfef4a40e2a51b6b5472d6ed47948fa2eeb1bd9
ManuEscr <manuel.latorre@est.fi.uncoma.edu.ar>
2023-03-30 12:15:24 -0300

    tresLeds

--- TresLeds/tresLeds.c
+++ TresLeds/tresLeds.c
@@ -5,38 +5,31 @@ int main(void)
   volatile unsigned char * PIN_B= (unsigned char *) 0X23;
 
   *(DDR_B)= 0b00100111;//bit 5= led arduino,bit 3= pinb (entrada)
-  *(PUERTO_B)= 0b00101111;//Habilita pullup en pines 1,2,3 pb0..pb1
-  //*(PIN_B)= 0b00001000;//indica que el pin 1,2 y 3 son entradas
-
- 
-
+  *(PUERTO_B)= 0b00001001;//Habilita pullup en pin pb3 y enciende pines pb0, pb1, pb2 
+  
   while(1){
     int bitIn = *(PIN_B) & 0b00001000;
+    volatile long i=0;
     if(bitIn==0){//Varia en funcion de la entrada (osea el pulsador)
-      //METER DELAY DE REBOTE DE PULSADOR
-      volatile long i=0;
       for(i=0; i<10000; i++){} 
-      
-      //DELAY
+     
       *(PUERTO_B)= *(PUERTO_B) & 0b11111110;//Apaga PB0=d8
       for(i=0; i<100000; i++){} //delay prendido
-      *(PUERTO_B)= *(PUERTO_B) || 0b00000010;//Prende
+      //delay(100000);
+      *(PUERTO_B)= *(PUERTO_B) | 0b00000010;//Prende PB1=d9
       
-      //DELAY
       for(i=0; i<100000; i++){} //delay prendido
+      //delay(100000);
       *(PUERTO_B)= *(PUERTO_B) & 0b11111101;//Apaga PB1=d9
       for(i=0; i<100000; i++){} //delay prendido
-      *(PUERTO_B)= *(PUERTO_B) || 0b00000100;//Prende
+      //delay(100000);
+      *(PUERTO_B)= *(PUERTO_B) | 0b00000100;//Prende PB2=d10
 
-      //Delay
       for(i=0; i<100000; i++){} //delay prendido
-      *(PUERTO_B)= *(PUERTO_B) & 0b11111011;//Apaga PB10
+      *(PUERTO_B)= *(PUERTO_B) & 0b11111011;//Apaga PB2=d10
       for(i=0; i<100000; i++){} //delay prendido
-      *(PUERTO_B)= *(PUERTO_B) || 0b00000001;//Prende 
-      for(i=0; i<10000; i++){} //delay 
+      *(PUERTO_B)= *(PUERTO_B) | 0b00000001;//Prende PB0=d8
+      for(i=0; i<100000; i++){} //delay prendido
     }
   }
-
-
-
 }
