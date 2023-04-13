de88e84416f544ba9559adff67617fc3278ed345
Manuel Latorre <manuel.latorre@est.fi.uncoma.edu.ar>
2023-04-13 11:53:14 -0300

    a

--- TresLeds/run.sh
+++
@@ -1,7 +0,0 @@
-avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o tresLeds.o tresLeds.c
-wait
-avr-gcc -mmcu=atmega328p tresLeds.o -o tresLeds.elf
-wait
-avr-objcopy -O ihex -R .eeprom tresLeds.elf tresLeds.hex
-wait
-avrdude -v v -v -v -p atmega328p -c arduino -P /dev/ttyUSB0 -b 115200 -D -U flash:w:tresLeds.hex:i
