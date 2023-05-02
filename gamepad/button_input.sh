7142baaea55b883baea7d3106b89046887a7d24e
Manuel Latorre <manuel.latorre@est.fi.uncoma.edu.ar>
2023-05-02 18:59:29 -0300

    dos botones, b no funca

--- gamepad/button_input.sh
+++ gamepad/button_input.sh
@@ -13,36 +13,17 @@ while read -n 1 tecla; do
     elif [ "$tecla" == "c" ]; then
         is_pressed_c=true  
         xdotool keydown "c"
-    elif [[ "$buffer" =~ qa ]]; then
+    elif [[ "$buffer" =~ aq ]]; then
         is_pressed_a=false   
         xdotool keyup "a"
         buffer=""
-    elif [[ "$buffer" =~ qb ]]; then
-        is_pressed_a=false   
+    elif [[ "$buffer" =~ bq ]]; then
+        is_pressed_b=false   
         xdotool keyup "b"
         buffer=""
-    elif [[ "$buffer" =~ qc ]]; then
+    elif [[ "$buffer" =~ cq ]]; then
         is_pressed_c=false   
         xdotool keyup "c"
         buffer=""
     fi
-
-    if [ "$is_pressed_a" == true ]; then
-        window=$(xdotool getwindowfocus) # obtener la identificación de la ventana activa
-        xdotool windowfocus $window # cambiar el foco a la ventana activa
-        xdotool keydown "a" # enviar la entrada simulada a la ventana activa
-    fi
-
-    if [ "$is_pressed_b" == true ]; then
-        window=$(xdotool getwindowfocus) # obtener la identificación de la ventana activa
-        xdotool windowfocus $window # cambiar el foco a la ventana activa
-        xdotool keydown "b" # enviar la entrada simulada a la ventana activa
-    fi
-
-    if [ "$is_pressed_c" == true ]; then
-        window=$(xdotool getwindowfocus) # obtener la identificación de la ventana activa
-        xdotool windowfocus $window # cambiar el foco a la ventana activa
-        xdotool keydown "c" # enviar la entrada simulada a la ventana activa
-    fi
-
 done < /dev/ttyUSB0
--- gamepad/gamepad.c
+++ gamepad/gamepad.c
@@ -10,7 +10,7 @@ int bit_in_der;
 void main(void)
 {
   *(DDR_B)= 0b00100000;//bit 5= led arduino,bit 3= pinb (entrada)
-  *(PUERTO_B)= 0b00001110;//Habilita pullup en pin pb3, pb2 y pb1
+  *(PUERTO_B)= 0b00000111;//Habilita pullup en pin pb3, pb2 y pb1
   serial_init();
   apretado_i=0;
   apretado_m=0;
@@ -19,37 +19,31 @@ void main(void)
  
   while(1){
     
-    bit_in_izq = *(PIN_B) & 0b00001000;
-    bit_in_med = *(PIN_B) & 0b00000100;
-    bit_in_der = *(PIN_B) & 0b00000010;
+    bit_in_izq = *(PIN_B) & 0b00000001;
+    bit_in_med = *(PIN_B) & 0b00000010;
+    bit_in_der = *(PIN_B) & 0b00000100;
     volatile long i=0;
-    if(bit_in_izq==0 /*&& apretado==0*/){//Si apretó (Lo comentado va junto al if del final)
+    if(bit_in_izq==0){//Si apretó (Lo comentado va junto al if del final)
       for(i=0; i<10000; i++){} 
       serial_put_char('a');
       *(PUERTO_B)= *(PUERTO_B) | 0b00100000;//Prende PB5=d13
       apretado_i=1;
     }
     
-    if(bit_in_med==0 /*&& apretado==0*/){//Si apretó (Lo comentado va junto al if del final)
+    if(bit_in_med==0){//Si apretó (Lo comentado va junto al if del final)
       for(i=0; i<10000; i++){} 
       serial_put_char('b');
       *(PUERTO_B)= *(PUERTO_B) & 0b00100000;
       apretado_m=1;
     }
 
-    if(bit_in_der==0 /*&& apretado==0*/){//Si apretó (Lo comentado va junto al if del final)
+    if(bit_in_der==0){//Si apretó (Lo comentado va junto al if del final)
       for(i=0; i<10000; i++){} 
       serial_put_char('c');
       *(PUERTO_B)= *(PUERTO_B) | 0b00100000;//Prende PB5=d13
       apretado_d=1;
     }
     soltar();
-    
-    /*if(bitIn!=0 && apretado==1){ //Si deje de pulsar y venia pulsando (esto para que no detecte inputs cuando mantiene apretado)
-      //*(PUERTO_B)= *(PUERTO_B) & 0b11011111;//Apaga PB5=d13
-      for(i=0; i<10000; i++){} 
-      apretado=0;
-    }*/
   }
 }
 
@@ -59,20 +53,17 @@ void soltar ()
   if(bit_in_izq!=0 && apretado_i==1){ 
       //*(PUERTO_B)= *(PUERTO_B) & 0b11011111;//Apaga PB5=d13
       for(i=0; i<10000; i++){} 
-      serial_put_string("qa");
-      serial_put_char(' ');
+      serial_put_string("aq");
       *(PUERTO_B)= *(PUERTO_B) & 0b11011111;
       apretado_i=0;
   }else if(bit_in_med!=0 && apretado_m==1){
     for(i=0; i<10000; i++){} 
-      serial_put_string("qb");
-      serial_put_char(' ');
+      serial_put_string("bq");
       *(PUERTO_B)= *(PUERTO_B) & 0b11011111;
       apretado_m=0;
   }else if(bit_in_der!=0 && apretado_d==1){
     for(i=0; i<10000; i++){} 
-      serial_put_string("qc");
-      serial_put_char(' ');
+      serial_put_string("cq");
       *(PUERTO_B)= *(PUERTO_B) & 0b11011111;
       apretado_d=0;
   }
