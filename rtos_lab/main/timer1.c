d26863e345681a527ee1bd8a2b1a15f1ff1c7d23
manuel <manuel@manuel.manuel>
2023-09-15 16:41:26 -0300

    interrupciones

--- rtos_lab/main/timer1.c
+++ rtos_lab/main/timer1.c
@@ -25,9 +25,29 @@
  * 2000000 t/s * 0.0025  = 3999  = 0x1194
  **********************************************************************/
 
+/********************** Calculos de valores ***************************
+ *
+ * f_cpu/prescalar = 16000000/8 = 2000000 t/s = 2 ticks por us
+ * 1000us/2t/us = 500 ticks necesarios para llegar a 1ms
+ * 1000 ticks necesarios para llegar a 2ms
+ * 500*20 = 10000 ticks para llegar a 20ms (FIN DE CICLO DEL SERVO)
+ * Tengo 500 posibles valores para indicar angulos
+ * 500 / 180 = 2.777
+ * Si recibo valores entre 0 y 180 hago 500 + (val*2.777) = cantidad de ticks necesarios (que va en el array de los servos)
+ *
+
+ **********************************************************************/
+
 /* Macros de valores */
-#define MIN_PWM_8P 0x03e8//0x07d0
-#define MAX_PWM_8P_SERVO 0x1130//0x0f9f
+#define TICKS_UNTIL_1ms 500
+#define TICKS_UNTIL_2ms 1000
+#define TICKS_UNTIL_20ms 10000
+#define TICK_OFFSET 2.777 // 0x07d0
+#define CLOCK_FREQ = 16000000
+#define PRESCALER = 8
+
+#define MIN_PWM_8P 0x03e8				// 0x07d0
+#define MAX_PWM_8P_SERVO 0x1130 // 0x0f9f
 #define MAX_PWM_8P_MOTOR 0x9c3f
 #define TIMER1_FREQ_H 0x9c
 #define TIMER1_FREQ_L 0x3f
@@ -37,18 +57,18 @@
 /* Estructura de datos del driver TIMER */
 typedef struct
 {
-        uint8_t control_reg_a;      // TCC1RA
-        uint8_t control_reg_b;      // TCC1RB
-        uint8_t control_reg_c;      // TCC1RC
-        uint8_t _reserved;          //
-        uint8_t counter_reg_l;      // TCNT1L
-        uint8_t counter_reg_h;      // TCNT1H
-        uint8_t in_capture_regl;    // ICR1L
-        uint8_t in_capture_regh;    // ICR1H
-        uint8_t out_compare_reg_al; // OCR1AL
-        uint8_t out_compare_reg_ah; // OCR1AH
-        uint8_t out_compare_reg_bl; // OCR1BL
-        uint8_t out_compare_reg_bh; // OCR1BH
+	uint8_t control_reg_a;			// TCC1RA
+	uint8_t control_reg_b;			// TCC1RB
+	uint8_t control_reg_c;			// TCC1RC
+	uint8_t _reserved;					//
+	uint8_t counter_reg_l;			// TCNT1L
+	uint8_t counter_reg_h;			// TCNT1H
+	uint8_t in_capture_regl;		// ICR1L
+	uint8_t in_capture_regh;		// ICR1H
+	uint8_t out_compare_reg_al; // OCR1AL
+	uint8_t out_compare_reg_ah; // OCR1AH
+	uint8_t out_compare_reg_bl; // OCR1BL
+	uint8_t out_compare_reg_bh; // OCR1BH
 } volatile timer1_t;
 volatile timer1_t *timer = (timer1_t *)0x80; // Direccion base
 
@@ -59,59 +79,23 @@ volatile int ticks;
 
 int timer1_init()
 {
-        /* setear la configuracion del timer1  */
-        timer->control_reg_a |= CONF_CONTROL_REG_A_FPWM;
-        timer->control_reg_b |= CONF_CONTROL_REG_B_FPWM;
-        timer->control_reg_c |= CONF_CONTROL_REG_C_FPWM;
+	/* setear la configuracion del timer1  */
+	timer->control_reg_a |= CONF_CONTROL_REG_A_FPWM;
+	timer->control_reg_b |= CONF_CONTROL_REG_B_FPWM;
+	timer->control_reg_c |= CONF_CONTROL_REG_C_FPWM;
 
-        /* determinar la frecuencia con el registro ICR  */
-        timer->in_capture_regh = TIMER1_FREQ_H;
-        timer->in_capture_regl = TIMER1_FREQ_L;
+	uint16_t ocr_value = 1; // Quiero una interrupción cada 2us
 
-        /* determinamos el ancho de la senial en alto en cada ciclo con el registro OCR1A */
-        timer->out_compare_reg_ah = 0;
-        timer->out_compare_reg_al = 0;
-
-        timer->out_compare_reg_bh = 0;
-        timer->out_compare_reg_bl = 0;
-        /* reiniciamos los registros del contador (por las dudas) */
-        timer->counter_reg_l = 0;
-        timer->counter_reg_h = 0;
-        return 0;
+	/* Determinamos el ancho de la señal en alto en cada ciclo con el registro OCR1A */
+	timer->out_compare_reg_ah = (uint8_t)(ocr_value >> 8); // Byte alto de OCR1A
+	timer->out_compare_reg_al = (uint8_t)(ocr_value & 0xFF); // Byte bajo de OCR1A
+//todo configurar timsk1
+	return 0;
 }
 
-/**
- * GRADE: para posicionar el servo (min 0 y max 180)
- */
-int timer1_servo(int grade)
-{
-        long int init_value, temp;
-        uint8_t low, high;
-
-        init_value = grade * 100 / 180;
-        temp = MIN_PWM_8P + (MAX_PWM_8P_SERVO - MIN_PWM_8P) / 100 * init_value;
-        high = (temp >> 8);
-        low = temp;
-
-        /* determinamos el ancho de la senial en alto en cada ciclo con el registro OCR1A */
-        timer->out_compare_reg_bh = high;
-        timer->out_compare_reg_bl = low;
-
-        return 0;
+// Función de interrupción del timer
+ISR(TIMER1_COMPA_vect) {
+    // Esta función se ejecutará cuando ocurra la interrupción de comparación de salida A
+    // Puedes realizar acciones basadas en el tiempo aquí
 }
 
-int timer1_motor(int speed)
-{
-    long int init_value, temp;
-        uint8_t low, high;
-
-        init_value = ((speed*(MAX_PWM_8P_MOTOR/10)));
-        high = (init_value >> 8);
-        low = init_value;
-
-        /* determinamos el ancho de la senial en alto en cada ciclo con el registro OCR1A */
-        timer->out_compare_reg_ah = high;
-        timer->out_compare_reg_al = low;
-
-        return 0;
-}
