c8673466f135dbbc37b1307430aef15e177124d2
manuel <manuel@manuel.manuel>
2023-10-15 15:19:16 -0300

    protocolo de servos

--- rtos_lab/main/main.c
+++ rtos_lab/main/main.c
@@ -14,7 +14,7 @@ int main(void)
 	*(DDR_B) |= 0b00101111; // bit 5= led arduino, pb0,1,2,3,4 = salidas de servos
 	*(PUERTO_B) |= 0b00101111;//Arranca con las señales todas en alto. El led encendido
 	//sleep(5);
-	timer1_init();
+	//timer1_init();
 	
 	/* creamos y ponemos a ejecutar las tareas */
 /* 	resume(create(main_rtc, 60, 30, "rtc", 0));
@@ -23,15 +23,77 @@ int main(void)
 	serial_put_str("\rRTOS_LAB INICIADO\r\n");
 /* 	sei(); */
 	print_array();
+	volatile int servo_index=-1;
+	volatile int direction=-1; //-1 = error, 0 = left, 1 = right
 	while (1)
 	{
 		if (serial_getchar_ready())
 		{
 			rcv_char = serial_get_char();
-			if (rcv_char == 'a' || rcv_char == 'd')
+			switch (rcv_char)
+			{
+				//SERVO 0
+				case 'q':
+					servo_index=0;
+					direction=0;
+					break;
+				case 'e':
+					servo_index=0;
+					direction=1;
+					break;
+				//SERVO 1
+				case 'a':
+					servo_index=1;
+					direction=0;
+					break;
+				case 'd':
+					servo_index=1;
+					direction=1;
+					break;
+				//SERVO 2
+				case 'z':	
+					servo_index=2;
+					direction=0;
+					break;
+				case 'c':
+					servo_index=2;
+					direction=1;
+					break;
+				//SERVO 3
+				case 'r':	
+					servo_index=3;
+					direction=0;
+					break;
+				case 'y':
+					servo_index=3;
+					direction=1;
+					break;
+				//SERVO 4
+				case 'f':	
+					servo_index=4;
+					direction=0;
+					break;
+				case 'h':
+					servo_index=4;
+					direction=1;
+					break;
+				default:
+					servo_index=-1;
+					direction=-1;
+					break;
+			}
+			//serial_put_int(servo_index);
+			adjust_servo_angle(rcv_char,servo_index, direction);
+			//Resetea variables para la siguiente iteracion
+			if(servo_index>-1 || direction>-1){
+				servo_index=-1;
+				direction=-1;
+			}
+			
+			/* if (rcv_char == 'a' || rcv_char == 'd')
 			{
 				adjust_servo_angle(rcv_char,0); //todo luego agregar la referencia al servo como param
-			}
+			} */
 		} 
 		
 		//sleepms(30); //!ver porque no funciona el sleepms
@@ -41,26 +103,22 @@ int main(void)
 	return 0;
 }
 
-void adjust_servo_angle(char rcv_char, int servo_index)
+void adjust_servo_angle(char rcv_char, int servo_index, int direction)
 {
-	if (rcv_char == 'a')
-	{
-		
-		if (servo_angles[servo_index] > 0)
+	if(servo_index>-1){
+
+		if (direction==0 && servo_angles[servo_index] > 0)
 		{
 			servo_angles[servo_index] -= 20;
 			servo_ticks[servo_index] = getTicksOffset(servo_angles[servo_index]);
 			print_array();
-		}
-	}
-	else if (rcv_char == 'd')
-	{
-		if (servo_angles[servo_index] < 180)
+		}else if (direction==1 && servo_angles[servo_index] < 180)
 		{
 			servo_angles[servo_index] += 20;
 			servo_ticks[servo_index] = getTicksOffset(servo_angles[servo_index]);
 			print_array();
 		}
+
 	}
 }
 
