db0481c2b4d8845ef97d36f805239941442490f5
manuel <manuel@manuel.manuel>
2023-11-08 13:41:33 -0300

    adornado el codigo

--- rtos_lab/main/main.c
+++ rtos_lab/main/main.c
@@ -10,11 +10,11 @@ extern int main_stick(void);
 int main(void)
 {
 	char rcv_char = ' ';
-	*(DDR_B) = 0b00111111; // bit 5= led arduino, pb0,1,2,3,4 = salidas de servos
+	*(DDR_B) = 0b00111111;		// bit 5= led arduino, pb0,1,2,3,4 = salidas de servos
 	*(PUERTO_B) = 0b00111111; // Arranca con las señales todas en alto.
-	*(DDR_D) = 0b00000000; // pd2 entrada
-	*(PUERTO_D) = 0b00000100; //Activo pullup
-	//sleep(5);
+	*(DDR_D) = 0b00000000;		// pd2 entrada
+	*(PUERTO_D) = 0b00000100; // Activo pullup
+	// sleep(5);
 	timer1_init();
 	serial_init();
 	adc_init();
@@ -22,11 +22,10 @@ int main(void)
 	resume(create(main_stick, 150, 30, "sti", 0));
 	/* resume(create(main_show_data, 60, 30, "sho", 0)); */
 
-	//sleep(1); //Le doy tiempo a las tareas a iniciar y que no se rompan los print
+	// sleep(1); //Le doy tiempo a las tareas a iniciar y que no se rompan los print
 	serial_put_str("\rRTOS_LAB INICIADO\r\n");
 	while (1)
 	{
-	
 	}
 	return 0;
 }
--- rtos_lab/main/stick.c
+++ rtos_lab/main/stick.c
@@ -7,34 +7,49 @@
 //Valores analogicos del stick. Defino la sensibilidad para evitar ajustes cuando se vuelve a centrar
 #define CENTERED_X 511
 #define CENTERED_Y 495
-#define SENS_X 30
-#define SENS_Y 30
+#define SENS_X 40
+#define SENS_Y 40
+
+#define CENTERED 500
+#define SENS 45
+#define RANGE_POS (CENTERED + SENS)
+#define RANGE_NEG (CENTERED - SENS)
 int apretado=0;
 int main_stick(void)
 {
-  int analog_in_x;
-  int analog_in_y;
-  int last_input_x=analog_in_x;
-  int last_input_y=analog_in_y;
+  int analog_in_xr;
+  int analog_in_yr;
+  int analog_in_xl;
+  int analog_in_yl;
+ /*  int last_input_xr = analog_in_xr;
+  int last_input_yr = analog_in_yr; */
   
- 
+  
   serial_put_str("\rStick start\r\n");
   while(1){
-    analog_in_x=adc_get(0); //Valores de 0 a 1023 !Este llega a 1020
-    analog_in_y=adc_get(1); //Valores de 0 a 1023 
+    analog_in_xr=adc_get(0); //Valores de 0 a 1023 !Este llega a 1020
+    analog_in_yr=adc_get(1); //Valores de 0 a 1023 
+    analog_in_xl=adc_get(2); //Valores de 0 a 1023 !Este llega a 1020
+    analog_in_yl=adc_get(3); //Valores de 0 a 1023 
 
     //Centered stick: (511, 495)
 
 
-    /* if(analog_in_x > last_input_x+2 || analog_in_x < last_input_x-2 || analog_in_y > last_input_y+2 || analog_in_y < last_input_y-2){
-      last_input_x=analog_in_x;
-      serial_put_int(analog_in_x, 4);
+    /* if(analog_in_xr > last_input_xr+2 || analog_in_xr < last_input_xr-2 || analog_in_yr > last_input_yr+2 || analog_in_yr < last_input_yr-2){
+      last_input_xr=analog_in_xr;
+      serial_put_int(analog_in_xr, 4);
     } */
-    if(analog_in_x > last_input_x+2 || analog_in_x < last_input_x-2 || analog_in_y > last_input_y+2 || analog_in_y < last_input_y-2){
-      last_input_x=analog_in_x;
-      last_input_y=analog_in_y;
-      set_mov_base(analog_in_x, analog_in_y);
-    }
+    /* if(analog_in_xr > last_input_xr+2 || analog_in_xr < last_input_xr-2 || analog_in_yr > last_input_yr+2 || analog_in_yr < last_input_yr-2){
+      last_input_xr=analog_in_xr;
+      last_input_yr=analog_in_yr;
+      set_mov_base(analog_in_xr, analog_in_yr);
+      //todo mando analog + numero servo y hago 4 llamadas a la misma funcion
+    } */
+
+    adjust_servo_angle(0, analog_in_xr);
+    adjust_servo_angle(1, analog_in_yr);
+    adjust_servo_angle(2, analog_in_xl);
+    adjust_servo_angle(3, analog_in_yl);
     stick_click();
     /* 
      Cuando se detecten variaciones en x o y llamar a metodo que detecte index y direction
@@ -44,7 +59,7 @@ int main_stick(void)
 }
 
 //SERVOS 0,1
-int set_mov_base(int analog_in_x, int analog_in_y){
+/* int set_mov_base(int analog_in_x, int analog_in_y){
   	volatile int servo_index_x=-1;
 	  volatile int direction_x=-1; //-1 = error, 0 = left, 1 = right
 
@@ -72,27 +87,31 @@ int set_mov_base(int analog_in_x, int analog_in_y){
       servo_index_y=1;
       direction_y=1;
     }
-
-    if(servo_index_y>-1 && direction_y>-1){
+    if(servo_index_y > -1 && direction_y > -1){
       adjust_servo_angle(servo_index_y, direction_y);
     }
-}
+} */
 
-void adjust_servo_angle(int servo_index, int direction)
+//todo aca recibir valor analogico y poner el if de linea 69 aca dentro
+void adjust_servo_angle(int servo_index, int analog_in)
 {
-	if(servo_index>-1){
-		if (direction==0 && servo_angles[servo_index] > 0)
-		{
-			servo_angles[servo_index] -= ANGLE_STEP;
-			servo_ticks[servo_index] = getTicksOffset(servo_angles[servo_index]);
-			print_array();
-		}else if (direction==1 && servo_angles[servo_index] < 100) //!Con rango [0,100] llega a 2ms
-		{
-			servo_angles[servo_index] += ANGLE_STEP;
-			servo_ticks[servo_index] = getTicksOffset(servo_angles[servo_index]);
-			print_array();
-		}
-	}
+  int direction = -1;
+  if(analog_in > (RANGE_POS)){
+    direction=0;
+  }else if(analog_in < (RANGE_NEG)){
+    direction=1;
+  }else{return}
+
+  if (direction==0 && servo_angles[servo_index] > 0)
+  {
+    servo_angles[servo_index] -= ANGLE_STEP;
+  }else if (direction==1 && servo_angles[servo_index] < 100) //!Con rango [0,100] llega a 2ms
+  {
+    servo_angles[servo_index] += ANGLE_STEP;
+  }
+  
+  servo_ticks[servo_index] = getTicksOffset(servo_angles[servo_index]);
+  print_array();
 }
 
 void print_array()
@@ -110,13 +129,11 @@ void print_array()
     serial_put_str_inline(", ");
 	}
   serial_put_str_inline("] ");
-	
 }
 
 int getTicksOffset(int angle)
 {
-  int result= TICKS_UNTIL_1ms + angle;
-	return result;
+	return TICKS_UNTIL_1ms + angle;
 }
 
 void stick_click(){
@@ -125,7 +142,7 @@ void stick_click(){
   int bitEncendido= *(PUERTO_B) & 0b00100000;//Obtengo estado
     
   if((bitIn == 0) /* && (apretado == 0) */){//Si apretó y no venia pulsando 
-    for(i=0; i<1000; i++){} 
+    /* for(i=0; i<1000; i++){}  */
     if(bitEncendido == 0){//Si estaba apagado lo prende
       cli();
       *(PUERTO_B) |= 0b00100000;//Prende PB5=d13
@@ -140,14 +157,13 @@ void stick_click(){
     
   }
 
-  if(bitIn!=0 && apretado==1){ //Si deje de pulsar y venia pulsando
-  for(i = 0; i < 1000; i++){}
-    cli();
+  /* if(bitIn!=0 && apretado==1){ //Si deje de pulsar y venia pulsando
+  /* for(i = 0; i < 1000; i++){} */
+    /* cli();
     *(PUERTO_B) &= 0b11011111;//Apaga PB5=d13
     sei();
-    
     apretado=0;
-  }
+  } */ 
 }
 
 
